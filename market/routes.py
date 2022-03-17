7f95a0e44221310642186da5f9154de526684fde
QuietWolf14 <jellydonut14@gmail.com>
2022-03-17 06:37:21 -0600

    add logout functionality

--- market/routes.py
+++ market/routes.py
@@ -2,7 +2,7 @@ from market import app, db
 from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 
 @app.route('/')
 @app.route('/home')
@@ -14,6 +14,7 @@ def about_page(username):
     return f'<h1>This is the about page of {username}</h1>'
 
 @app.route('/market')
+@login_required
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
@@ -26,7 +27,9 @@ def register_page():
                               email=form.email_address.data,
                               password=form.password1.data)
         db.session.add(user_to_create)
-        db.session.commit()
+        db.session.commit()        
+        login_user(user_to_create)
+        flash(f'Account created successfully! You are now logged in as {user_to_create.username}', category='success')
         return redirect(url_for('market_page'))
     if form.errors != {}: #If there are errors from the validations
         for err_msg in form.errors.values():
@@ -47,3 +50,9 @@ def login_page():
             flash('Incorrect credentials', category='danger')
 
     return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("You have been logged out", category='info')
+    return redirect(url_for("home_page"))
