b2343bef63caabe32dc389b401bbde6653e34438
QuietWolf14 <jellydonut14@gmail.com>
2022-03-19 22:14:18 -0600

    add purchase functionality

--- market/models.py
+++ market/models.py
@@ -16,7 +16,7 @@ class User(db.Model, UserMixin):
     @property
     def prettier_budget(self):
         if len(str(self.budget)) >= 4:
-            return f'{str(self.budget)[:-3]},{str(self.budget)[-3:]}'
+            return f'${str(self.budget)[:-3]},{str(self.budget)[-3:]}'
         else:
             return f"${self.budget}"
 
@@ -31,6 +31,9 @@ class User(db.Model, UserMixin):
     def check_password_correction(self, attempted_password):
         return bcrypt.check_password_hash(self.password_hash, attempted_password)
 
+    def can_purchase(self, item_obj):
+        return self.budget >= item_obj.price
+
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
@@ -41,3 +44,8 @@ class Item(db.Model):
 
     def __repr__(self):
         return f'Item {self.name}'
+
+    def buy(self, user):
+        self.owner = user.id
+        user.budget -= self.price
+        db.session.commit()
